d1bc5a40d40e5d9e250a28a1bb5e85d41bc52f69
Matteo Cafasso <noxdafox@gmail.com>
2018-04-16 00:29:27 +0300

    mix: release 0.1.0
    Features:
      - Added `x-cache-persistence` configuration value to enable choosing
        whether to store the cache in memory or disk.
    
    Fixes:
      - Fix a bug preventing the deletion of exchanges.
      - Fix the expired cache entries loop, raised period from 3ms to 3s.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule RabbitExchangeTypeMessageDeduplication.Mixfile do
 
     [
       app: :rabbitmq_message_deduplication_exchange,
-      version: "0.0.1",
+      version: "0.1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps_path: deps_dir,
