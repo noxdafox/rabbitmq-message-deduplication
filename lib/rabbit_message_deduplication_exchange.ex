720cfb298b56ed42edbd9b1ce0d955c06d4321c0
Matteo Cafasso <noxdafox@gmail.com>
2018-04-22 22:07:03 +0300

    exchange: add exchange info callback implementation
    Provides to the User access to cache information (size and number of
    entries).
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- lib/rabbit_message_deduplication_exchange.ex
+++ lib/rabbit_message_deduplication_exchange.ex
@@ -154,11 +154,15 @@ defmodule RabbitMQ.MessageDeduplicationExchangeType do
     RabbitExchange.assert_args_equivalence(exchange, args)
   end
 
-  def info(_x) do
-    []
+  def info(exchange) do
+    info(exchange, [:cache_info])
   end
 
-  def info(_x, _y) do
+  def info(exchange(name: name), [:cache_info]) do
+    [cache_info: name |> cache_name() |> RabbitMQ.Cache.info()]
+  end
+
+  def info(_ex, _it) do
     []
   end
 
