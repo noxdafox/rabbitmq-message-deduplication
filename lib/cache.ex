623ca248361487c2db39d1d7c3a0534424e0c6c2
Matteo Cafasso <noxdafox@gmail.com>
2018-04-22 11:36:45 +0300

    add license headers
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- lib/cache.ex
+++ lib/cache.ex
@@ -1,3 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2017-2018, Matteo Cafasso.
+# All rights reserved.
+
+
 defmodule RabbitMQ.Cache do
   @moduledoc """
   Simple cache implemented on top of Mnesia.
--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,3 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2017-2018, Matteo Cafasso.
+# All rights reserved.
+
+
 defmodule RabbitMQ.CacheSupervisor do
   @moduledoc """
   The Cache Supervisor supervises the Cache GenServer Processes.
