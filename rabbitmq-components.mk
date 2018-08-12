a0af666d37ec8e800913240cce545d39f3f1606a
Matteo Cafasso <noxdafox@gmail.com>
2018-08-12 13:53:42 +0300

    rabbitmq-components.mk: fetch makefile from rabbitmq-common
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- rabbitmq-components.mk
+++ rabbitmq-components.mk
@@ -1,12 +1,12 @@
 # Inspired by erlang.mk bootstrap Makefile.
-# Fetch updated rabbitmq-components.mk from rabbitmq-server.
+# Fetch updated rabbitmq-components.mk from rabbitmq-common.
 
-RABBITMQ_SERVER_DIR ?= .rabbitmq-components.mk.build
+RABBITMQ_COMMON_DIR ?= .rabbitmq-components.mk.build
 
 rabbitmq-components.mk: rabbitmq-components-bootstrap
-	git clone https://github.com/rabbitmq/rabbitmq-server $(RABBITMQ_SERVER_DIR)
-	cp $(RABBITMQ_SERVER_DIR)/rabbitmq-components.mk ./rabbitmq-components.mk
-	rm -rf $(RABBITMQ_SERVER_DIR)
+	git clone https://github.com/rabbitmq/rabbitmq-common $(RABBITMQ_COMMON_DIR)
+	cp $(RABBITMQ_COMMON_DIR)/mk/rabbitmq-components.mk ./rabbitmq-components.mk
+	rm -rf $(RABBITMQ_COMMON_DIR)
 
 .PHONY: rabbitmq-components-bootstrap
 rabbitmq-components-bootstrap: ;
