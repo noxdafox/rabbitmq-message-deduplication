0814588b578437da2546c53a6a91230192c769e6
Matteo Cafasso <noxdafox@gmail.com>
2019-04-13 00:18:02 +0300

    issue #28, #30: fix cache tests
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- test/cache_test.exs
+++ test/cache_test.exs
@@ -10,7 +10,7 @@ defmodule RabbitMQ.MessageDeduplicationPlugin.Cache.Test do
 
   alias :timer, as: Timer
   alias :mnesia, as: Mnesia
-  alias RabbitMQ.MessageDeduplicationPlugin.Cache, as: MessageCache
+  alias RabbitMQ.MessageDeduplicationPlugin.Cache, as: Cache
 
   setup do
     cache = :test_cache
@@ -27,102 +27,99 @@ defmodule RabbitMQ.MessageDeduplicationPlugin.Cache.Test do
     cache_options = [size: 1, ttl: nil, persistence: :memory]
     cache_ttl_options = [size: 1, ttl: Timer.seconds(1), persistence: :memory]
 
-    start_supervised!(%{id: cache,
-                        start: {MessageCache,
-                                :start_link,
-                                [cache, cache_options]}})
-    start_supervised!(%{id: cache_ttl,
-                        start: {MessageCache,
-                                :start_link,
-                                [cache_ttl, cache_ttl_options]}})
-    start_supervised!(%{id: cache_simple,
-                        start: {MessageCache,
-                                :start_link,
-                                [cache_simple, cache_simple_options]}})
+    :ok = Cache.create(cache, cache_options)
+    :ok = Cache.create(cache_ttl, cache_ttl_options)
+    :ok = Cache.create(cache_simple, cache_simple_options)
 
     %{cache: cache, cache_ttl: cache_ttl, cache_simple: cache_simple}
   end
 
   test "basic insertion",
       %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
+    {:ok, :exists} = Cache.insert(cache, "foo")
   end
 
   test "TTL at insertion",
       %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo", Timer.seconds(1))
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo", Timer.seconds(1))
+    {:ok, :exists} = Cache.insert(cache, "foo")
 
     1 |> Timer.seconds() |> Timer.sleep()
 
-    :ok = MessageCache.insert(cache, "foo")
+    :ok = Cache.delete_expired_entries(cache)
+
+    {:ok, :inserted} = Cache.insert(cache, "foo")
   end
 
   test "TTL at table creation",
       %{cache: _, cache_ttl: cache, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
+    {:ok, :exists} = Cache.insert(cache, "foo")
 
     1 |> Timer.seconds() |> Timer.sleep()
 
-    :ok = MessageCache.insert(cache, "foo")
+    :ok = Cache.delete_expired_entries(cache)
+
+    {:ok, :inserted} = Cache.insert(cache, "foo")
   end
 
   test "entries are deleted after TTL",
       %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo", Timer.seconds(1))
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo", Timer.seconds(1))
+    {:ok, :exists} = Cache.insert(cache, "foo")
 
     Timer.sleep(3200)
+
+    :ok = Cache.delete_expired_entries(cache)
 
     {:atomic, []} = Mnesia.transaction(fn -> Mnesia.all_keys(cache) end)
   end
 
   test "entries are deleted if cache is full",
       %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
-    :ok = MessageCache.insert(cache, "bar")
-    {:error, :already_exists} = MessageCache.insert(cache, "bar")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
+    {:ok, :exists} = Cache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "bar")
+    {:ok, :exists} = Cache.insert(cache, "bar")
 
-    :ok = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
   end
 
   test "cache entry deletion", %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
+    {:ok, :exists} = Cache.insert(cache, "foo")
 
-    MessageCache.delete(cache, "foo")
+    Cache.delete(cache, "foo")
 
-    :ok = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
   end
 
   test "cache information",
       %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
 
-    [size: 1, bytes: _, entries: 1] = MessageCache.info(cache)
+    [size: 1, bytes: _, entries: 1] = Cache.info(cache)
   end
 
   test "simple cache information",
       %{cache: _, cache_ttl: _, cache_simple: cache_simple} do
-    :ok = MessageCache.insert(cache_simple, "foo")
+    {:ok, :inserted} = Cache.insert(cache_simple, "foo")
 
-    [bytes: _, entries: 1] = MessageCache.info(cache_simple)
+    [bytes: _, entries: 1] = Cache.info(cache_simple)
   end
 
   test "flush the cache", %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.insert(cache, "foo")
-    {:error, :already_exists} = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
+    {:ok, :exists} = Cache.insert(cache, "foo")
 
-    :ok = MessageCache.flush(cache)
+    :ok = Cache.flush(cache)
 
-    :ok = MessageCache.insert(cache, "foo")
+    {:ok, :inserted} = Cache.insert(cache, "foo")
   end
 
   test "drop the cache", %{cache: cache, cache_ttl: _, cache_simple: _} do
-    :ok = MessageCache.drop(cache)
+    :ok = Cache.drop(cache)
 
     assert Enum.member?(Mnesia.system_info(:tables), cache) == false
   end
