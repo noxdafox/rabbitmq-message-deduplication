698a3f414c8f3b7bdd055bd79b770b7b3784af5f
Matteo Cafasso <noxdafox@gmail.com>
2021-03-24 23:47:27 +0200

    Update rabbitmq-components.mk
    As per commit https://github.com/rabbitmq/rabbitmq-server/commit/a63f169fcbd443802d20ec8c88ed4ca298d19e3b,
    use the new centralized rabbitmq-components.mk file.

--- rabbitmq-components.mk
+++ rabbitmq-components.mk
@@ -4,8 +4,8 @@
 RABBITMQ_COMMON_DIR ?= .rabbitmq-components.mk.build
 
 rabbitmq-components.mk: rabbitmq-components-bootstrap
-	git clone https://github.com/rabbitmq/rabbitmq-common $(RABBITMQ_COMMON_DIR)
-	cp $(RABBITMQ_COMMON_DIR)/mk/rabbitmq-components.mk ./rabbitmq-components.mk
+	git clone https://github.com/rabbitmq/rabbitmq-server $(RABBITMQ_COMMON_DIR)
+	cp $(RABBITMQ_COMMON_DIR)/rabbitmq-components.mk ./rabbitmq-components.mk
 	rm -rf $(RABBITMQ_COMMON_DIR)
 
 .PHONY: rabbitmq-components-bootstrap
