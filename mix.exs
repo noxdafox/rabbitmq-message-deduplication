4d2f6f00eb6c9ebd0fd1538913f28c39796df117
Matteo Cafasso <noxdafox@gmail.com>
2024-11-10 18:23:41 +0200

    mix: release 0.6.4
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RabbitMQ.MessageDeduplicationPlugin.Mixfile do
   def project do
     [
       app: :rabbitmq_message_deduplication,
-      version: "0.6.3",
+      version: "0.6.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
