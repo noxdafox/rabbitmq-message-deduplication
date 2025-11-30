a1b16ab45bd6a14276143cf0d41be5761e5c5f90
Matteo Cafasso <noxdafox@gmail.com>
2025-11-30 15:17:51 +0200

    build(mix): release 0.7.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RabbitMQ.MessageDeduplicationPlugin.Mixfile do
   def project() do
     [
       app: :rabbitmq_message_deduplication,
-      version: "0.7.1",
+      version: "0.7.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
